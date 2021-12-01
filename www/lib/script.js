bddba493367a5a85f862bfadae5411fc2633bc3a
Ace871 <diego.souza286@etec.sp.gov.br>
2021-12-01 09:26:15 +0900

    Implementação qrcode, plugins de alert e plugin de verificação de internet

--- www/lib/script.js
+++ www/lib/script.js
@@ -1,4 +1,4 @@
-window.onload = function(){
+window.onload = function (){
   const cadastrar = document.querySelector("#cadastrar");
   const nome = document.querySelector("#nome");
   const curso = document.querySelector("#curso");
@@ -6,80 +6,122 @@ window.onload = function(){
   const id = document.querySelector("#id");
   const alterar = document.querySelector("#alterar");
   const deletar = document.querySelector("#deletar");
+  const code = document.querySelector("#qrcode");
 
-  // Ação de Cadastro de Pessoa e Curso
+
+  //ação de cadastrar uma pessoa e curso
 
   cadastrar.addEventListener("click", function(){
-    let formdata = new Formdata();
-    formdata.append('nome',`${nome.value}`);
-    formdata.append('curso',`${curso.value}`);
+      let formData = new FormData();
+      formData.append('nome', `${nome.value}`);
+      formData.append('curso', `${curso.value}`);
 
-    fetch("https://www.jussimarleal.com.br/exemplo_api/pessoa",
-    {
-      body: formdata,
-      method:"post",
-      mode:'cors',
-      cache:'default'
-    }).then(()=>{
-      alert("Registro Efetuado com Sucesso");
-      LimparCampos();
+      fetch("https://www.jussimarleal.com.br/exemplo_api/pessoa",
+          {
+              body: formData,
+              method: "post",
+              mode: 'cors',
+              cache: 'default'
+          }).then(alert("Cadastrado"));
     });
+
+
+  //metodo que lista uma pessoa
+
+  buscar.addEventListener("click", function(){   
+    navigator.connection.type;
+    if(navigator.connection.type == Connection.NONE){
+      function onConfirm(buttonIndex) {
+        function onConfirm(buttonIndex) {
+      alert('You selected button ' + buttonIndex);
+}
+    }
+      navigator.notification.confirm(
+        'Você está sem net, saia ou volte', // message
+        onConfirm,            // callback to invoke with index of button pressed
+        'Sem Internet',           // title
+        ['Tentar novamente','Sair']     // buttonLabels
+    );
+    }else{
+    fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`,
+    {
+      method:"get",
+      mode: 'cors',
+      cache: 'default'
+    })
+      .then(response => {response.json()
+        .then(data => {
+          nome.value = data['nome'];
+          curso.value = data['curso'];
+        })
+      });
+    }
   });
 
-  //Listar Cadastros
 
-  buscar.addEventListener("click", function(){
-    fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`),{
-    method:"get",
-    mode:'cors',
-    cache:'default'
-    }).then(response=>{
-      response.json().then(data => {
-        nome.value = data['nome'];
-        curso.value = data['curso'];
-      })
+  //metodo para deletar o registro 
+
+    deletar.addEventListener("click", function(){
+      fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`,{
+        method:"get",
+        mode:'cors',
+        cache:'default'
+      }).then(()=>{
+      alert("Registro deletado");
+      limparCampos();
+      });
     })
-  })
 
-  //Alterar Cadastros
-
-  alterar.addEventListener("click", function(){
-    fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`),{
-    method:"put",
-    mode:'cors',
-    cache:'default',
-    headers:{
-      'content-type':'application/json; charset=UTF-8'
-    },
-    body:JSON.stringify({
-      'nome':`${nome.value}`,
-      'curso':`${curso.value}`
+  
+  //metodo para alterar os dados dos registros
+  
+    alterar.addEventListener("click", function(){
+       fetch(`https://www.jussimarleal.com.br/exemplo_api/pessoa/${id.value}`,{
+        method:"put",
+        mode:'cors',
+        cache:'default',
+        headers:{
+          'Conntent-type': 'application/json; charset=UTF-8'
+        },
+        body:JSON.stringify({
+          'nome': `${nome.value}`,
+          'curso': `${curso.value}`
+        })
+      }).then(()=>{
+        alert("registro alterado com sucesso");
+        limparCampos();
+      });
     })
-  }).then(()=>{
-    alert("Registro Alterado com Sucesso")
-    LimparCampos();
-    });
+
+
+  //ação de limpar os campos
+
+    function limparCampos (){
+      nome.value = "";
+      curso.value = "";
+    }
+  code.addEventListener("click", function(){
+       cordova.plugins.barcodeScanner.scan(
+      function (result) {
+          id.value = result.text;
+      },
+      function (error) {
+          alert("Scanning failed: " + error);
+      },
+      {
+          preferFrontCamera : false, // iOS and Android
+          showFlipCameraButton : true, // iOS and Android
+          showTorchButton : true, // iOS and Android
+          torchOn: false, // Android, launch with the torch switched on (if available)
+          saveHistory: true, // Android, save scan history (default false)
+          prompt : "Place a barcode inside the scan area", // Android
+          resultDisplayDuration: 500, // Android, display scanned text for X ms. 0 suppresses it entirely, default 1500
+          formats : "QR_CODE,PDF_417", // default: all but PDF_417 and RSS_EXPANDED
+          orientation : "portait", // Android only (portrait|landscape), default unset so it rotates with the device
+          disableAnimations : true, // iOS
+          disableSuccessBeep: false // iOS and Android
+      }
+    );
   });
-
-  //Deletar Registros
-
-  deletar.addEventListener("click", function(){
-    fetch("https://www.jussimarleal.com.br/exemplo_api/pessoa",
-    {
-      body: formdata,
-      method:"delete",
-      mode:'cors',
-      cache:'default'
-    }).then(()=>{
-        alert("Registro Deletado com Sucesso!");
-        LimparCampos();
-    });
-  })
-
-  //Limpar Campos
-
-  function LimparCampos(){
-    nome.value = "";
-    curso.value = "";
-  };
+  
 }
